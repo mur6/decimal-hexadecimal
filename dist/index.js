42b51ae75d878e11a58a69be5d35c0ade4d4f9f4
mur6 <mur6@users.noreply.github.com>
2021-06-11 06:32:04 +0000

    deploy: 2b22a585e8a48559be529d29bc7093b12130a5a5

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,6 @@
+import * as __SNOWPACK_ENV__ from '../_snowpack/env.js';
+import.meta.env = __SNOWPACK_ENV__;
+
 import App from './App.svelte.js';
 
 let app = new App({
@@ -5,3 +8,12 @@ let app = new App({
 });
 
 export default app;
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
+if (undefined /* [snowpack] import.meta.hot */ ) {
+    undefined /* [snowpack] import.meta.hot */ .accept();
+    undefined /* [snowpack] import.meta.hot */ .dispose(() => {
+        app.$destroy();
+    });
+}
